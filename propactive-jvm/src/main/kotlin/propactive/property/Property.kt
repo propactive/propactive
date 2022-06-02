6cf824abb9966ba9d4b6ae23ff15d6e15ed21f06
U-ways <uways.eid@sky.uk>
2022-06-02 09:59:03 +0100

    Add annotation Kdocs
    U-ways

--- propactive-jvm/src/main/kotlin/propactive/property/Property.kt
+++ propactive-jvm/src/main/kotlin/propactive/property/Property.kt
@@ -8,22 +8,51 @@ import kotlin.annotation.AnnotationTarget.PROPERTY
 import kotlin.reflect.KClass
 
 /**
- * TODO
+ * Marks the field as an application property.
+ * All fields annotated with @Property will be part of the final application properties file.
+ *
+ * Example:
+ * ```
+ * @Property(["test: value"])
+ * const val property = "app.resource.key"
+ * ```
+ * will generate the following line in an application properties file:
+ * ```
+ * app.resource.key=value
+ * ```
  */
 @Target(PROPERTY, FIELD)
 @Retention(RUNTIME)
 @MustBeDocumented
 annotation class Property(
     /**
-     * TODO
+     * Property value for each given environment delimited with a colon (:)
+     * You can reuse a value for multiple environments using the forward slash (/) delimiter, example:
+     * ```
+     * @Property(["test/stage/prod: value"])
+     * ```
+     * Will generate an application property for 3 environments (test, stage, and prod)
      */
     val value: Array<String> = [""],
     /**
-     * TODO
+     * The type of the property value. (used on runtime for validation)
+     * Current natively supported types:
+     * - BASE64
+     * - BOOLEAN
+     * - DECIMAL
+     * - INTEGER
+     * - JSON
+     * - STRING
+     * - URI
+     * - URL
+     * - UUID
+     *
+     * You can use the interface [Type] to create your own type for custom validation.
      */
     val type: KClass<out Type> = STRING::class,
     /**
-     * TODO
+     * Marks a property as mandatory.
+     * Any property marked as mandatory cannot have blank values within any environment.
      */
     val mandatory: Boolean = true,
 )
