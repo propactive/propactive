9c0fe05e1dde1347d49578dd4b9de1a8ceae785a
u-ways <ue95.career@gmail.com>
2022-09-06 16:19:51 +0100

    Sign only if it's a versioned-release build as -SNAPSHOT plugin versions are not supported for Gradle Plugin Portal...
    U-ways

--- propactive-plugin/build.gradle.kts
+++ propactive-plugin/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     val pluginPublishVersion = "0.21.0"
 
+    id("signing")
     id("java-gradle-plugin")
     id("com.gradle.plugin-publish") version pluginPublishVersion
 }
 
 apply(plugin = "org.jetbrains.kotlin.jvm")
+
+val isDevVersion = "$version" == "DEV-SNAPSHOT"
+val isSemVersioned = "$version".matches(Regex("v[0-9]+\\.[0-9]+\\.[0-9]+.*?"))
+val isVersionedSnapshot = isSemVersioned && "$version".endsWith("-SNAPSHOT")
+val isVersionedRelease = isSemVersioned && isVersionedSnapshot.not()
 
 gradlePlugin {
     plugins {
@@ -29,4 +35,16 @@ dependencies {
 
     implementation(gradleApi())
     testImplementation(gradleTestKit())
+}
+
+signing {
+    // Sign only if it's a versioned-release build as -SNAPSHOT plugin versions
+    // not supported for Gradle Plugin Portal...
+    setRequired { isVersionedRelease }
+    if (isRequired) useGpgCmd()
+    sign(configurations.archives.get())
+}
+
+tasks {
+    publishPlugins { onlyIf { isVersionedRelease } }
 }
