8cabf271c77a78400a6d6fd1683d905749fc4804
U-ways <uways.eid@sky.uk>
2022-09-30 20:08:36 +0100

    Add documentation to the registered Configuration plugin extension
    U-ways

--- propactive-plugin/src/main/kotlin/io/github/propactive/plugin/Configuration.kt
+++ propactive-plugin/src/main/kotlin/io/github/propactive/plugin/Configuration.kt
@@ -1,5 +1,13 @@
 package io.github.propactive.plugin
 
+/**
+ * Propactive configurations.
+ *
+ * @param environments Comma separated list of environments to generate the properties (i.e. test,stage,prod)
+ * @param implementationClass Sets the location of your properties object (i.e. com.package.path.to.your.ApplicationProperties)
+ * @param destination Sets the location of your generated properties file within the build directory.
+ * @param filenameOverride Allows overriding given filename for an environment. (This should only be used when generating application properties for a singular environment)
+ */
 open class Configuration(
     var environments:        String? = null,
     var implementationClass: String? = null,
