517e3d7695b824c8cfb02a87a27b1be4e1738a06
u-ways <ue95.career@gmail.com>
2022-10-25 02:31:37 +0100

    Use versioned catalog for dependencies & plugins
    U-ways

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,6 @@ include(
     ":propactive-jvm",
 )
 
-plugins {
-    // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.50.2"
-}
-
 dependencyResolutionManagement {
     enableFeaturePreview("VERSION_CATALOGS")
     versionCatalogs {
