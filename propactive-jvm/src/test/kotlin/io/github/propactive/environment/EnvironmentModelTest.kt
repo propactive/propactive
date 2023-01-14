c12784dcdbb43e96ddd4eefe53f4da435105e740
u-ways <ue95.career@gmail.com>
2023-01-14 15:54:22 +0000

    #45 [U-ways] Add more environment model equality tests coverage

--- propactive-jvm/src/test/kotlin/io/github/propactive/environment/EnvironmentModelTest.kt
+++ propactive-jvm/src/test/kotlin/io/github/propactive/environment/EnvironmentModelTest.kt
@@ -1,5 +1,8 @@
 package io.github.propactive.environment
 
+import io.github.propactive.property.PropertyModel
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import nl.jqno.equalsverifier.EqualsVerifier
 import nl.jqno.equalsverifier.Warning
 import org.junit.jupiter.api.Test
@@ -13,5 +16,28 @@ internal class EnvironmentModelTest {
             .suppress(Warning.ALL_FIELDS_SHOULD_BE_USED)
             .forClass(EnvironmentModel::class.java)
             .verify()
+    }
+
+    @Test
+    fun `should be equal when the name is the same`() {
+        val sameName = "sameName"
+        val sameFilename = "sameFilename"
+        val sameProperties = setOf<PropertyModel>()
+
+        val environmentA = EnvironmentModel(sameName, sameFilename, sameProperties)
+        val environmentB = EnvironmentModel(sameName, sameFilename, sameProperties)
+
+        environmentA shouldBe environmentB
+    }
+
+    @Test
+    fun `should not be equal when the name is different`() {
+        val sameFilename = "sameFilename"
+        val sameProperties = setOf<PropertyModel>()
+
+        val environmentA = EnvironmentModel("name", sameFilename, sameProperties)
+        val environmentB = EnvironmentModel("differentName", sameFilename, sameProperties)
+
+        environmentA shouldNotBe environmentB
     }
 }
