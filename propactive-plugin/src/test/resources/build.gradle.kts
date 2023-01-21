0b478f22edcbcd97aa13017947d3b6425ca687d6
u-ways <ue95.career@gmail.com>
2023-01-21 15:05:47 +0000

    #53 [U-ways] Create GradleRunner integration tests for Kotlin JVM

---
+++ propactive-plugin/src/test/resources/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.8.0"
+    val propactiveVersion = "DEV-SNAPSHOT"
+
+    id("java-library")
+    id("io.github.propactive") version propactiveVersion
+    kotlin("jvm") version kotlinVersion
+}
+
+propactive {
+    implementationClass = "ApplicationProperties"
+    filenameOverride = "application.properties"
+    destination = layout.buildDirectory.dir("properties").get().asFile.absolutePath
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal { url = uri("file://${System.getProperty("user.home")}/.m2/repository") }
+}
+
+dependencies {
+    val propactiveVersion = "DEV-SNAPSHOT"
+    val log4jKotlinVersion = "1.2.0" // TODO: Upgrade to 1.3.0 if my PR is merged
+    val log4jVersion = "2.19.0"
+
+    implementation("io.github.propactive:propactive-jvm:$propactiveVersion")
+    implementation("org.apache.logging.log4j:log4j-api-kotlin:$log4jKotlinVersion")
+    implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+}
+
+tasks {
+    wrapper {
+        distributionType = ALL
+    }
+
+    withType<KotlinCompile>().configureEach {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
